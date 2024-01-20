abc92ba153328800ad64425ec1654e3737a2cf25
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-01-20 11:03:16 -0800

    refactor: use icon for github source

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from "./ThemeProvider";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Github } from "lucide-react";
 
 function NavBar() {
 	const { theme, setTheme } = useTheme();
@@ -8,16 +8,15 @@ function NavBar() {
 			<nav className="flex items-center justify-between flex-wrap p-6 bg-slate-400 dark:bg-[#181818]">
 				<div className="flex items-center flex-shrink-0 mr-6 gap-3">
 					<span className="text-xl text-neutral-950 dark:text-white">Reddit Image Viewer</span>
-					<div className="h-6 bg-black dark:bg-white w-0.5"></div>
-					<a
-						href="https://github.com/toommyliu/reddit-image-viewer"
-						target="_blank"
-						rel="noreferrer"
-						className="text-xl text-neutral-950 dark:text-white"
-					>
-						Github
-					</a>
 				</div>
+				<button className="ml-auto">
+					<Github
+						className="text-white h-[1.2rem] w-[1.2rem] mr-4"
+						onClick={() => {
+							window.open("https://github.com/toommyliu/reddit-image-viewer");
+						}}
+					/>
+				</button>
 				<button className="text-white" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
 					{theme === "light" ? (
 						<Sun className="h-[1.2rem] w-[1.2rem] text-neutral-950" />
