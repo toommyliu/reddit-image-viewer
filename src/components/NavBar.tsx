fe6b9258842c23e9e399459c8d519124135a2605
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-01-20 14:52:57 -0800

    refactor: improve navbar color scheme

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,13 +5,13 @@ function NavBar() {
 	const { theme, setTheme } = useTheme();
 	return (
 		<>
-			<nav className="flex items-center justify-between flex-wrap p-6 bg-slate-400 dark:bg-[#181818]">
+			<nav className="flex items-center justify-between flex-wrap p-6 bg-stone-600 dark:bg-[#181818]">
 				<div className="flex items-center flex-shrink-0 mr-6 gap-3">
-					<span className="text-xl text-neutral-950 dark:text-white">Reddit Image Viewer</span>
+					<span className="text-xl text-white">Reddit Image Viewer</span>
 				</div>
 				<button className="ml-auto">
 					<Github
-						className="dark:text-white h-[1.2rem] w-[1.2rem] mr-4"
+						className="text-white h-[1.2rem] w-[1.2rem] mr-4"
 						onClick={() => {
 							window.open("https://github.com/toommyliu/reddit-image-viewer");
 						}}
@@ -19,7 +19,7 @@ function NavBar() {
 				</button>
 				<button className="text-white" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
 					{theme === "light" ? (
-						<Sun className="h-[1.2rem] w-[1.2rem] text-neutral-950" />
+						<Sun className="h-[1.2rem] w-[1.2rem] text-white" />
 					) : (
 						<Moon className="h-[1.2rem] w-[1.2rem]" />
 					)}
