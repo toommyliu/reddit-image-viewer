66688ac7d3c61cf1d882220cd665c899cc42e959
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-01-20 11:17:22 -0800

    chore: attribute icon copyrights to lucide

--- src/icons/sun.tsx
+++ src/icons/sun.tsx
@@ -1,3 +1,4 @@
+// Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
 import type { FC } from "react";
 
 type SunProps = React.SVGProps<SVGSVGElement>;
