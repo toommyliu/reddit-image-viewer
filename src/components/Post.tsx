a5d9119cb49668ef2c9aaf384cf3b8f775f44345
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-01-20 14:49:48 -0800

    refactor: apply dark bg to post titles

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -8,11 +8,11 @@ type PostProps = {
 const Post = ({ img, title }: PostProps) => {
 	return (
 		<>
-			<div className="w-72 bg-white rounded border-2">
+			<div className="w-72 bg-white dark:dark:bg-[#1f1f1f] rounded border-2">
 				<img src={img} alt="post" className="h-80 w-72 object-cover rounded-t" />
 				<div className="px-4 py-3 w-256 ">
 					<div className="border-b-2 border-gray-200 mb-2"></div>
-					<span className="mr-3 mx-auto text-xs text-black">{title}</span>
+					<span className="mr-3 mx-auto text-xs text-black dark:text-white">{title}</span>
 				</div>
 			</div>
 		</>
