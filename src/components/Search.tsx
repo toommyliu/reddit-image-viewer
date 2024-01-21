4abcb119785c4e1a21970740dba8ed2efb42bd76
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-01-20 21:11:57 -0800

    wip: partially functional infinite scroll

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,29 +1,83 @@
 import { Shuffle, Trash2, ExternalLink } from "lucide-react";
-import { useState, type FormEvent } from "react";
-import type { SubredditResponse } from "../types";
+import { useState, type FormEvent, useEffect, useCallback } from "react";
 import { shuffle, subreddit, username } from "../utils";
 import Post from "./Post";
-import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Search() {
 	const [query, setQuery] = useState("");
 	const [mode, setMode] = useState("user");
 	const [loading, setLoading] = useState(false);
 	const [posts, setPosts] = useState<{ title: string; url: string }[]>([]);
-	const [success, setSuccess] = useState(true);
+	const [page, setPage] = useState(1);
+	const [submit, setSubmit] = useState(false);
 
-	const onModeChange = (event: FormEvent<HTMLSelectElement>) => {
-		event.preventDefault();
-		// @ts-expect-error value exists
-		setMode(event.target.value as string);
+	// load 9 posts at a time
+	const loadPosts = useCallback(() => {
+		if (!submit) {
+			return;
+		}
+		if (query == "") {
+			return;
+		}
+
+		const arr: typeof posts = [];
+
+		let url = mode === "user" ? username(query) : subreddit(query);
+		url += `?limit=${9 * page}`;
+		console.log(`url: ${url}`);
+
+		fetch(`${url}`)
+			.then((res) => {
+				if (res.status !== 200) {
+					return;
+				}
+				return res.json();
+			})
+			.then((res) => {
+				if (!res) {
+					return;
+				}
+				console.log(res);
+				/* eslint-disable */
+				const startIndex = (page - 1) * 9;
+				for (const child of res.data.children) {
+					const is_image = child.data["post_hint"] == "image";
+					if (is_image) {
+						const obj = { title: child.data["title"], url: child.data["url"] };
+						arr.push(obj);
+					}
+				}
+				const endIndex = startIndex + arr.length;
+				/* eslint-enable */
+				setPosts((prev) => [...prev, ...arr.slice(startIndex, endIndex)]);
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	}, [mode, page, query, submit]);
+
+	useEffect(() => {
+		loadPosts();
+	}, [loadPosts]);
+
+	const handleScroll = () => {
+		if (
+			window.innerHeight + document.documentElement.scrollTop + 1 >=
+			document.documentElement.scrollHeight
+		) {
+			console.log("incrementing page");
+			setLoading(true);
+			setPage((previous) => previous + 1);
+		}
 	};
 
-	const onQueryChange = (event: FormEvent<HTMLInputElement>) => {
-		event.preventDefault();
-		// @ts-expect-error value exists
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-		setQuery((event.target.value as string).toLowerCase());
-	};
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
 
 	const onFormSubmit = (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
@@ -32,54 +86,10 @@ export default function Search() {
 			return;
 		}
 
-		setSuccess(true);
 		setLoading(true);
 		setPosts([]);
-
-		let url = mode === "user" ? username(query) : subreddit(query);
-		url += ""; // ?limit=100
-
-		// TODO: finish infinite scrolling
-
-		console.log(`url: ${url}`);
-
-		// TODO: better error handling
-		fetch(`${url}`)
-			.then((res) => {
-				if (res.status !== 200) {
-					setSuccess(false);
-					return null;
-				}
-				return res.json();
-			})
-			.then((data: SubredditResponse) => {
-				if (!data) {
-					return;
-				}
-				console.log(data);
-				const { children } = data.data;
-				for (const child of children) {
-					const is_image = child.data["post_hint"] == "image";
-					if (is_image) {
-						setPosts((posts) => [
-							...posts,
-							{ title: child.data["title"], url: child.data["url"] },
-						]);
-					}
-				}
-				setSuccess(true);
-			})
-			.catch((err) => {
-				if ("reason" in err && "message" in err && "error" in err) {
-					if (err["error"] === 404) {
-						alert("404: Not Found");
-					}
-				}
-				console.log(err);
-			})
-			.finally(() => {
-				setLoading(false);
-			});
+		setSubmit(true);
+		loadPosts();
 	};
 
 	return (
@@ -92,7 +102,9 @@ export default function Search() {
 						<select
 							className="border-2 border-gray-300 rounded-lg focus:outline-none mr-2 mt-10 text-center"
 							value={mode}
-							onChange={onModeChange}
+							onChange={(event) => {
+								setMode(event.target.value);
+							}}
 						>
 							<option value="user">User</option>
 							<option value="subreddit">Subreddit</option>
@@ -100,14 +112,16 @@ export default function Search() {
 						<form onSubmit={onFormSubmit}>
 							<input
 								value={query}
-								onChange={onQueryChange}
+								onChange={(event) => {
+									event.preventDefault();
+									setQuery(event.target.value.toLowerCase());
+								}}
 								placeholder="Query"
 								className="border-2 border-gray-300 rounded-lg mt-10 pl-3"
 							/>
 						</form>
 					</div>
 				)}
-				{!success && <span className="dark:text-white mt-10">Failed to find any results!</span>}
 				{posts.length > 0 && (
 					<div className="space-x-5">
 						<button
@@ -138,24 +152,13 @@ export default function Search() {
 					</div>
 				)}
 			</div>
-			{!loading && posts.length > 0 && (
-				<>
-					<InfiniteScroll
-						dataLength={posts.length}
-						next={() => {
-							alert("we need more!");
-						}}
-						hasMore={true}
-						loader={<h4>Loading...</h4>}
-					>
-						<section className="w-fit mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center justify-center gap-y-5 gap-x-10 mt-10 mb-5">
-							{posts.map((post, idx) => (
-								<Post img={post.url} title={post.title} key={`post-${idx}`} />
-							))}
-						</section>
-					</InfiniteScroll>
-				</>
-			)}
+			<>
+				<section className="w-fit mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center justify-center gap-y-5 gap-x-10 mt-10 mb-5">
+					{posts.map((post, idx) => (
+						<Post img={post.url} title={post.title} key={`post-${idx}`} />
+					))}
+				</section>
+			</>
 		</>
 	);
 }
