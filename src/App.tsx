f10a711df73f1ebe272c35d1780961ae466f0572
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-01-20 19:37:29 -0800

    refactor: use fragment

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import Search from "./components/Search";
 
 function App() {
 	return (
-		<div className="dark:bg-[#1f1f1f] min-h-screen">
+		<>
 			<ThemeProvider>
 				<NavBar />
 				<Search />
 			</ThemeProvider>
-		</div>
+		</>
 	);
 }
 
