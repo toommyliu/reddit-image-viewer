f9c5956bdacd489380abfc7b8d827c2a2c8aa97c
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-01-24 15:01:55 -0800

    refactor: clean slate, init shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import NavBar from "./components/NavBar";
-import { PostProvider } from "./components/PostProvider";
-import SearchField from "./components/SearchField";
-import { ThemeProvider } from "./components/ThemeProvider";
-
-const queryClient = new QueryClient();
-
 export default function App() {
 	return (
 		<>
-			<QueryClientProvider client={queryClient}>
-				<ReactQueryDevtools />
-				<ThemeProvider>
-					<NavBar />
-					<PostProvider>
-						<SearchField />
-					</PostProvider>
-				</ThemeProvider>
-			</QueryClientProvider>
+			<div className="font-bold text-2xl">Hello World</div>
 		</>
 	);
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,6 @@
-export const makeUserUrl = (user: string) => `https://www.reddit.com/user/${user}.json`;
-export const makeSubredditUrl = (sub: string) => `https://www.reddit.com/r/${sub}.json`;
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export const shuffle = <T>(og_arr: T[]): T[] => {
-	const arr = og_arr.slice();
-	for (let i = arr.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[arr[i], arr[j]] = [arr[j], arr[i]];
-	}
-	return arr;
-};
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
